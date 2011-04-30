c345e66cae8deab180b7d18b701214e32cea8682
Daemotron <mail@daemotron.net>
2011-04-30 22:29:09 +0200

    added page module

--- src/unisite/page/models.py
+++ src/unisite/page/models.py
@@ -1,3 +1,57 @@
-from django.db import models
+'''
+Copyright (c) 2011 Daemotron <mail@daemotron.net>
 
-# Create your models here.
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
+from unisite.util.rst import render
+from unisite.util.rst import ent_replace 
+
+#from my_universe.base.models import Language
+
+# The page class represents a pseudo-static page
+class Page(models.Model):
+    name = models.CharField(max_length=200, verbose_name=_('page name'), unique=True)
+    default_slug = models.SlugField(max_length=64, verbose_name=_('page default slug'))
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(verbose_name=_('publishing date'))
+
+    def __unicode__(self):
+        return self.name
+
+
+class PageTranslation(models.Model):
+    page = models.ForeignKey(Page, verbose_name=_('page'))
+    language = models.CharField(max_length=2, choices=settings.LANGUAGES, verbose_name=_('language'))
+    slug = models.SlugField(max_length=64, verbose_name=_('page slug (URL)'))
+    title = models.CharField(max_length=200, verbose_name=_('page title'))
+    heading = models.CharField(max_length=200, verbose_name=_('page heading'))
+    content = models.TextField(verbose_name=_('page content (reStructuredText)'))
+    author = models.ForeignKey(User, verbose_name=_('author\'s name'))
+    content_html = models.TextField(editable=False, blank=True)
+    title_html = models.CharField(max_length=255, editable=False, blank=True)
+    heading_html = models.CharField(max_length=255, editable=False, blank=True)
+
+    class Meta:
+        unique_together = ("language", "slug")
+        
+    def save(self, *args, **kwargs):
+        self.content_html = render(self.content, self.language)
+        self.title_html = ent_replace(self.title, self.language)
+        self.heading_html = ent_replace(self.heading, self.language)
+        super(PageTranslation, self).save(*args, **kwargs)
--- src/unisite/page/views.py
+++ src/unisite/page/views.py
@@ -1,1 +1,58 @@
-# Create your views here.
+'''
+Copyright (c) 2011 Daemotron <mail@daemotron.net>
+
+Permission to use, copy, modify, and distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+'''
+
+from django.http import Http404
+from unisite.util import response
+
+from unisite.page import models
+
+
+def get_page(slug='index', lang='en'):
+    try:
+        pg = models.PageTranslation.objects.get(slug__iexact=slug, language__iexact=lang)
+    except models.PageTranslation.DoesNotExist:
+        try:
+            pg = models.PageTranslation.objects.get(slug__iexact=slug)
+        except models.PageTranslation.DoesNotExist:
+            try:
+                p = models.Page.objects.get(default_slug__iexact=slug)
+            except models.Page.DoesNotExist:
+                return False
+
+            try:
+                pg = models.PageTranslation.objects.get(page=p.id)
+            except models.PageTranslation.DoesNotExist:
+                return False
+
+    return(pg)
+
+
+def index(request):
+    return(show(request, 'index'))
+
+
+def show(request, page_name):
+    pg = get_page(page_name, request.LANGUAGE_CODE)
+    if (pg):
+        ctx = {
+            'language': pg.language,
+            'title': pg.title_html,
+            'heading': pg.heading_html,
+            'content': pg.content_html,
+        }
+        return response.render(request, 'page/show.html', ctx)
+    else:
+        raise Http404
