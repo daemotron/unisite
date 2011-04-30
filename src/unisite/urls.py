79b73112706dba0b2e4ef2810c68112b3c58b32b
Daemotron <mail@daemotron.net>
2011-04-30 20:52:29 +0200

    Added basic frame for unisite applications

--- src/unisite/urls.py
+++ src/unisite/urls.py
@@ -1,17 +1,22 @@
 from django.conf.urls.defaults import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'unisite.views.home', name='home'),
-    # url(r'^unisite/', include('unisite.foo.urls')),
+    # Index is index...
+    url(r'^$', 'my_universe.page.views.index', name="index-page"),
+
+    # Page module
+    url(r'^page/', include('unisite.page.urls')),
+    
+    # Blog module
+    url(r'^blog/', include('unisite.blog.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
